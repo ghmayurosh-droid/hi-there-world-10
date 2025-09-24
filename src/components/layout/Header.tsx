da91d3dfc2dab86db275e9d6a198da5d87df7279
ghmayurosh-droid <gh.mayur.osh@gmail.com>
2025-09-25 04:13:55 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
-import logoMain from "@/assets/bollywood-logo-main.png";
+import logoMain from "@/assets/BP1.png";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,7 +26,7 @@ const Header = () => {
             <img 
               src={logoMain} 
               alt="Bollywood Paathshaala by Shahid Hasan" 
-              className="h-26 w-auto"
+              className="h-46 w-46"
             />
           </Link>
 
